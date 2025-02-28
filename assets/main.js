1d4404793b01c2334c0636eeb07568554cbf1eee
NavodContrath <navod.contrath@gmail.com>
2025-02-28 12:51:29 +0100

    add js with fetch and render function

---
+++ assets/main.js
@@ -0,0 +1,27 @@
+const displayerEl = document.querySelector(".row")
+
+fetch('https://lanciweb.github.io/demo/api/pictures/')
+    .then(response => response.json())
+    .then(data => renderCards(data))
+    .catch(error => console.log(error))
+
+function renderCards(info) {
+    info.forEach(card => {
+        const markup = `                
+        <div class="col-sm-12 my-5">
+                <div class="card_1">
+                   <div class="card_top">
+                        <img src="/assets/img/pin.svg" id="pin" alt="pin">
+                        <img src="${card.url}" id="dogo" alt="dog_img">
+                    </div>
+                    <div class="card_bot my-3">
+                        <div>${card.date}</div>
+                        <h2>${card.title.toUpperCase()}</h2>
+                        
+                    </div>
+                </div>
+        </div>`
+        displayerEl.insertAdjacentHTML("beforeend", markup)
+
+    });
+}
